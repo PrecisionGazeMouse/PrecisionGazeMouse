14492bb4f284664a5eff1848025ef4ad0e7e03f6
QXD-me <qxdmeisme@gmail.com>
2021-03-21 13:49:18 +0000

    Add config to skip eViacam prompt

--- PrecisionGazeMouse/Properties/Settings.Designer.cs
+++ PrecisionGazeMouse/Properties/Settings.Designer.cs
@@ -12,7 +12,7 @@ namespace PrecisionGazeMouse.Properties {
     
     
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
-    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Microsoft.VisualStudio.Editors.SettingsDesigner.SettingsSingleFileGenerator", "16.2.0.0")]
+    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Microsoft.VisualStudio.Editors.SettingsDesigner.SettingsSingleFileGenerator", "16.8.1.0")]
     internal sealed partial class Settings : global::System.Configuration.ApplicationSettingsBase {
         
         private static Settings defaultInstance = ((Settings)(global::System.Configuration.ApplicationSettingsBase.Synchronized(new Settings())));
@@ -152,6 +152,18 @@ namespace PrecisionGazeMouse.Properties {
             }
             set {
                 this["eViacamKey"] = value;
+            }
+        }
+        
+        [global::System.Configuration.UserScopedSettingAttribute()]
+        [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
+        [global::System.Configuration.DefaultSettingValueAttribute("False")]
+        public bool SkipeViacamPrompt {
+            get {
+                return ((bool)(this["SkipeViacamPrompt"]));
+            }
+            set {
+                this["SkipeViacamPrompt"] = value;
             }
         }
     }
